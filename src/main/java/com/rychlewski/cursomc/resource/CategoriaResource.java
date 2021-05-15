e2f6cf58a003b653645b98552394b5f6699277d8
MarcosRychlewski <vinirychlewski@gmail.com>
2021-05-15 11:43:59 -0300

    list

--- src/main/java/com/rychlewski/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/rychlewski/cursomc/resource/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.rychlewski.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rychlewski.cursomc.domain.Categoria;
 
 
 //RestController anotaçao de controlador do app
@@ -12,9 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
+	
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "metodo esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 	
 	
