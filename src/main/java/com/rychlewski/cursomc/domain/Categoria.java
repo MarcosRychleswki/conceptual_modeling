3a0b1481e58721904a262ba7e437b7d26bab4ba9
MarcosRychlewski <vinirychlewski@gmail.com>
2021-05-15 12:11:35 -0300

    first table

--- src/main/java/com/rychlewski/cursomc/domain/Categoria.java
+++ src/main/java/com/rychlewski/cursomc/domain/Categoria.java
@@ -2,12 +2,23 @@ package com.rychlewski.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // equals e hashCode sao uma forma de comparar os objetos pelos seus valores
+// (strategy = GenerationType.IDENTITY) === geracao automatica dos IDs
 
 
+
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
